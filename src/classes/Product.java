fc8f8ddc69a0816332d96a1a41f97760e4f512bf
Wellington Garcia <garciawell@gmail.com>
2020-02-26 23:04:09 -0300

    Creating class Product

---
+++ src/classes/Product.java
@@ -0,0 +1,7 @@
+package classes;
+
+public class Product {
+    String name;
+    double price;
+    double discount;
+}
---
+++ src/classes/ProductTest.java
@@ -0,0 +1,26 @@
+package classes;
+
+public class ProductTest {
+    public static void main(String[] args) {
+       Product p1 = new Product();
+       p1.name = "Notebook";
+       p1.price = 4556.89;
+       p1.discount = 0.25;
+
+       Product p2 = new Product();
+       p2.name = "Caneta Preta";
+       p2.price = 12.56;
+       p2.discount = 0.15;
+
+        System.out.println(p1.name);
+        System.out.println(p2.name);
+
+        double finalPrice1 = p1.price * (1 - p1.discount);
+        double finalPrice2 = p2.price * (1 - p2.discount);
+
+        double averageCart = (finalPrice1 + finalPrice2) / 2;
+
+        System.out.printf("Média do Carrinho = R$%.2f", averageCart );
+
+    }
+}
