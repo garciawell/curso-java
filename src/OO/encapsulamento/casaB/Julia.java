6aa54d860c507369b02b226e7dd4c669251a7dde
Wellington Garcia <garciawell@gmail.com>
2020-03-30 20:10:38 -0300

    Exercicios para fixar encapsulamento public - private - protect- default

---
+++ src/OO/encapsulamento/casaB/Julia.java
@@ -0,0 +1,16 @@
+package OO.encapsulamento.casaB;
+
+import OO.encapsulamento.casaA.Ana;
+
+public class Julia {
+
+    void testeAcessos(){
+
+        Ana sogra = new Ana();
+
+//        System.out.println(sogra.segredo);
+//        System.out.println(sogra.facoDentroDeCasa);
+//        System.out.println(sogra.formaDeFalar);
+        System.out.println(sogra.todosSabem);
+    }
+}
---
+++ src/OO/encapsulamento/casaB/Pedro.java
@@ -0,0 +1,16 @@
+package OO.encapsulamento.casaB;
+
+import OO.encapsulamento.casaA.Ana;
+
+public class Pedro extends Ana {
+
+    void testeAcessos(){
+
+//        Ana mae = new Ana();
+
+//        System.out.println(mae.segredo);
+//        System.out.println(mae.facoDentroDeCasa);
+        System.out.println(formaDeFalar);
+        System.out.println(todosSabem);
+    }
+}
