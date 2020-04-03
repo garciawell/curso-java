84255eee389cb39ccd4f4018f130705001205755
Wellington Garcia <garciawell@gmail.com>
2020-04-02 23:51:46 -0300

    Foreach Funcional com mais flexibilidade

--- src/lambdas/Foreach.java
+++ src/lambdas/Foreach.java
@@ -16,13 +16,19 @@ public class Foreach {
         }
 
         System.out.println("\nLambda #01...");
-
         aprovados.forEach(nome -> {
             System.out.println(nome + "!!!");
         });
 
+        System.out.println("\nLambda #02...");
+        aprovados.forEach(Foreach::meuImprimir);
+
 
         System.out.println("\nMethod Reference...");
         aprovados.forEach(System.out::println);
+    }
+
+    static void meuImprimir(String nome) {
+        System.out.println("Oi! meu nome é " + nome);
     }
 }
